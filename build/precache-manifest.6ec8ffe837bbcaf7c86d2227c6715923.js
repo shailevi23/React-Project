ec82a58ed0e56dc145799794df9f047c8e83c1d0
shailevi23 <shailevi23@gmail.com>
2022-01-01 19:22:55 +0200

    New repositoty to send

---
+++ build/precache-manifest.6ec8ffe837bbcaf7c86d2227c6715923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9878218fbc06e2dd7291e99ef3389c2",
+    "url": "/React-Project-Moveo/index.html"
+  },
+  {
+    "revision": "8144ef6f8cf88475205f",
+    "url": "/React-Project-Moveo/static/css/main.f36dd28d.chunk.css"
+  },
+  {
+    "revision": "9729a5cb3a7e82fd9a09",
+    "url": "/React-Project-Moveo/static/js/2.895223e9.chunk.js"
+  },
+  {
+    "revision": "8144ef6f8cf88475205f",
+    "url": "/React-Project-Moveo/static/js/main.e7d35841.chunk.js"
+  },
+  {
+    "revision": "967e01b5b178b839ab21",
+    "url": "/React-Project-Moveo/static/js/runtime~main.2eca5d75.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from "./AppRouter";
-import { BrowserRouter as Router,  } from "react-router-dom";
 import './index.css';
 
-
-ReactDOM.render(
-<React.StrictMode>
-    <Router>
-        <AppRouter />
-    </Router>
-</React.StrictMode>, 
-document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
